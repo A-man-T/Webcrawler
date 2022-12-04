3d1a8c60d5fed1fe1d42b4df7e7e26ff2a4f320c
amantewari <amanatewari@gmail.com>
2022-12-03 21:33:55 -0600

    token

--- src/main/java/assignment/Token.java
+++ src/main/java/assignment/Token.java
@@ -1,4 +1,29 @@
 package assignment;
 
-public class Token {
+public class Token { }
+
+class AndToken extends Token {
+    @Override
+    public String toString() {
+        return "AndToken";
+    }
+}
+
+class OrToken extends Token {
+    @Override
+    public String toString() {
+        return "AndToken";
+    }
+}
+class LeftParenToken extends Token {
+    @Override
+    public String toString() {
+        return "AndToken";
+    }
+}
+class RightParenToken extends Token {
+    @Override
+    public String toString() {
+        return "AndToken";
+    }
 }
