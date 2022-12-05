6ae36f312592fcbb58ffc676629bcd3d012331b2
amantewari <amanatewari@gmail.com>
2022-12-05 14:05:15 -0600

    i think we done

--- src/test/java/assignment/generateWeb.java
+++ src/test/java/assignment/generateWeb.java
@@ -28,4 +28,31 @@ public class generateWeb {
         }
     }
 
+
+    @Test
+    public void checkWeb() throws ClassNotFoundException, IOException {
+        WebCrawler.main(new String[] {"file:/Users/amantewari/Turing/prog7/testWeb/1.txt"});
+        WebQueryEngine wqe = WebQueryEngine.fromIndex((WebIndex) Index.load("index.db"));
+
+        /*
+        Scanner scan = new Scanner(new File("test_queries_green.txt"));
+        Scanner answerScanner = new Scanner(new File("green_answers.txt"));
+        System.out.println("Start Here:            ");
+        ArrayList<Integer> results = new ArrayList<>();
+        while(scan.hasNext()) {
+            String query = scan.nextLine();
+            try {
+                assertEquals(wqe.query(query).size(),answerScanner.nextInt());
+                //results.add(wqe.query(query).size());
+            }
+            catch(Exception e){
+                System.err.print("failed on this one");
+                System.err.println(query);
+            }
+            }
+
+         */
+
+    }
+
 }
